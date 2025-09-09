fba4dec1859ca501b51cc29418f20359223d33cc
Raushan Sinha <raushankrsinha2004@gmail.com>
2025-09-09 20:45:34 +0530

    Tech Data file created and edited

--- src/pages/TechStack.jsx
+++ src/pages/TechStack.jsx
@@ -1,40 +1,7 @@
 import CodeIcon from '@mui/icons-material/Code';
+import { heading, codingTools } from '../data/techStacksData';
 
 export default function TechStack() {
-    const heading = 'Tech Stack';
-
-    //todo: Frontend Tools & heading
-    const frontendTools = [
-        {
-            heading: 'Frontend Development',
-            tools: ['JavaScript', 'React.js', 'Next.js', 'Tailwind CSS', 'Bootstrap', 'HTML5', 'CSS3']
-        }
-    ];
-
-    //todo: Languages 
-    const languages = [
-        {
-            heading: 'Programming Languages',
-            lang: ['C', 'C++', 'Java', 'JavScript']
-        }
-    ];
-
-    //todo: Version Control
-    const versionCtrl = [
-        {
-            heading: 'Version Control',
-            tools: ['Git', 'GitHub']
-        }
-    ];
-
-    //todo: Coding IDE & AI Tools
-    const code_AiTools = [
-        {
-            heading: 'Coding IDE & AI Tools',
-            tools: ['VS Code', 'Cursor', 'GitHub Copilot', 'ChatGPT', 'Claude', 'Grok', 'DeepSeek', 'Replit']
-        }
-    ];
-
     return (
         <>
             {/* Header */}
@@ -47,66 +14,15 @@ export default function TechStack() {
                         <div className="heading text-2xl font-mono">{heading}</div>
                     </div>
 
-                    {/* Frontend Tools */}
-                    {frontendTools.map((category, idx) => (
-                        <div className="tech-stacks mb-4" key={idx}>
+                    {/* Coding Tools */}
+                    {codingTools.map((category) => (
+                        <div className="tech-stacks mb-4" key={category.id}>
                             <p className="font-sans text-lg mb-2 font-bold">{category.heading}</p>
                             <div className="toolsName flex flex-wrap gap-2">
                                 {category.tools.map((tool, i) => (
                                     <span
                                         key={i}
                                         className="tools bg-slate-800 px-3 py-1 rounded-full text-sm font-mono hover:bg-sky-600 focus:outline-2 focus:outline-offset-2 focus:outline-sky-500 active:bg-sky-7000"
-                                    >
-                                        {tool}
-                                    </span>
-                                ))}
-                            </div>
-                        </div>
-                    ))}
-
-                    {/*Programming Languages */}
-                    {languages.map((category, idx) => (
-                        <div className="tech-stacks mb-4" key={idx}>
-                            <p className="font-sans text-lg mb-2 font-bold">{category.heading}</p>
-                            <div className="toolsName flex flex-wrap gap-2">
-                                {category.lang.map((tool, i) => (
-                                    <span
-                                        key={i}
-                                        className="tools bg-slate-800 px-3 py-1 rounded-full text-sm font-mono hover:bg-sky-600 focus:outline-2 focus:outline-offset-2 focus:outline-sky-500 active:bg-sky-700"
-                                    >
-                                        {tool}
-                                    </span>
-                                ))}
-                            </div>
-                        </div>
-                    ))}
-
-                    {/* Version Control */}
-                    {versionCtrl.map((category, idx) => (
-                        <div className="tech-stacks mb-4" key={idx}>
-                            <p className="font-sans text-lg mb-2 font-bold">{category.heading}</p>
-                            <div className="toolsName flex flex-wrap gap-2">
-                                {category.tools.map((tool, i) => (
-                                    <span
-                                        key={i}
-                                        className="tools bg-slate-800 px-3 py-1 rounded-full text-sm font-mono hover:bg-sky-600 focus:outline-2 focus:outline-offset-2 focus:outline-sky-500 active:bg-sky-700"
-                                    >
-                                        {tool}
-                                    </span>
-                                ))}
-                            </div>
-                        </div>
-                    ))}
-
-                    {/* Coding IDE & AI Tools */}
-                    {code_AiTools.map((category, idx) => (
-                        <div className="tech-stacks mb-4" key={idx}>
-                            <p className="font-sans text-lg mb-2 font-bold">{category.heading}</p>
-                            <div className="toolsName flex flex-wrap gap-2">
-                                {category.tools.map((tool, i) => (
-                                    <span
-                                        key={i}
-                                        className="tools bg-slate-800 px-3 py-1 rounded-full text-sm font-mono hover:bg-sky-600 focus:outline-2 focus:outline-offset-2 focus:outline-sky-500 active:bg-sky-700"
                                     >
                                         {tool}
                                     </span>
